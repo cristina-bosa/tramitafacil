04249a83f67e4654804546210989d42ee2cad849
Cristina Bosa <cristinabosasanchez@gmail.com>
2023-03-29 19:15:00 +0100

    Changes on css and notfout

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,8 @@
 <template>
   <section class="error-404">
     <img src="../assets/error-404.png" alt="img-not-found" />
-    <section>
-      <h1 class="text-primary text-black">Error 404 - Página no encontrada</h1>
-      <p>Lo sentimos, la página que estás buscando no existe.</p>
-      <router-link to="/"><button class="btn-primary ">Volver a inicio</button></router-link>
-    </section>
+    <h1 class="text-black text-center">¡Oh no! Parece que perdimos esta página en el laberinto de internet</h1>
+    <router-link to="/"><button class="btn-primary">Volver</button></router-link>
   </section>
 </template>
 <script>
@@ -13,3 +10,14 @@ export default {
   name: "NotFound",
 };
 </script>
+<style scoped>
+.error-404 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  gap: 1rem;
+}
+</style>
